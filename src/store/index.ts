e6cef5089f2ff714a1e39e6ab4bd5d8829dab63b
Chung941211 <444192287@qq.com>
2020-02-27 18:25:21 +0800

    feat add configureStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,21 @@
 import { combineReducers } from 'redux';
+import { all, fork } from 'redux-saga/effects';
+
+
 import { IndexState } from './index/types';
+import { indexReducer } from './index/reducer';
+import indexSaga from './index/saga';
 
 export interface stateObject {
     index: IndexState
+}
+
+export const reducerRoot = () => {
+    combineReducers({
+        index: indexReducer
+    })
+}
+
+export function* sagaRoot () {
+    yield all([fork(indexSaga)])
 }
--- src/store/index/saga.ts
+++ src/store/index/saga.ts
@@ -1,2 +1,25 @@
 import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
+import { IndexActionType } from './types';
+import { indexFetchSucceeded, indexFetchFailed } from './action';
+import { Apis } from '../../utils/http';
 
+function* getPlaylist () {
+  try {
+
+    const result = yield call(Apis, 'get', '/related/playlist');
+    
+    yield put(indexFetchSucceeded(result));
+
+  } catch (err) {
+    yield put(indexFetchFailed(err));
+  }
+}
+
+
+function* watchRequeset () {
+    yield takeEvery(IndexActionType.INDEX_FETCH_REQUESTED, getPlaylist)
+}
+
+export default function* indexSaga () {
+    yield all([fork(watchRequeset)])
+}
