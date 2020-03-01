ab59ebca2300124676e10b6a80ec5fbde4f2aaec
Chung941211 <444192287@qq.com>
2020-03-01 23:24:13 +0800

    feat Personalized

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,22 +1,64 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Personalized } from '../../store/index/types';
+import { indexFetchRequst } from '../../store/index/action';
+import { stateObject } from '../../store'
 
-export interface headerProps { 
-    name: String; 
+import './index.scss';
+
+interface personalizedState {
+  data: Personalized[],
+  loading: Boolean
 }
 
-export default class Index extends Component<headerProps, {}> {
+interface personalizedDispatch {
+  indexFetchRequst: typeof indexFetchRequst
+}
 
-  componentWillMount () {
-    console.log('test');
+type indexProps = personalizedState & personalizedDispatch
+
+class Index extends React.Component<indexProps> {
+    
+  public componentDidMount() {
+    const { indexFetchRequst: fr } = this.props;
+
+    fr();
   }
 
-  render () {
-    const { name } = this.props;
+  public render () {
+    const { data } = this.props;
     return (
-        <div className="video">
-          1
-            { name }
+        <div>
+          <div className="remd">推荐歌单</div>
+
+            <div className="remd_ul">
+              {
+                data.map(item => 
+                  <div className="remd_li">
+                    <div className="remd_img">
+                      <img src={item.picUrl} alt=""/>
+                        <span className="u-earp remd_lnum">{item.playCount}</span>
+                    </div>
+                    <p className="remd_text">{item.name}</p>
+                  </div>
+                )
+              }
+            </div>
         </div>
     )
   }
 }
+
+const mapStateToProps = ({ index } : stateObject) => ({
+    loading: index.loading,
+    data: index.data
+})
+
+const mapDispatchToProps = {
+    indexFetchRequst
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Index);
---
+++ src/pages/index/personalized.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+class Personalized extends React.Component {
+
+    public componentDidMount() {
+    }
+    
+    public render () {
+        return (
+            <div>1</div>
+        )
+    }
+}
+
+
+export default Personalized
