a02a8ec34926a51008d2d6e2ba6ac1f8fac7fd2d
Chung941211 <444192287@qq.com>
2020-03-03 22:23:57 +0800

    fix personalized

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,64 +1,21 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { Personalized } from '../../store/index/types';
-import { indexFetchRequst } from '../../store/index/action';
-import { stateObject } from '../../store'
+import PersonalizedList from './personalized'
 
 import './index.scss';
 
-interface personalizedState {
-  data: Personalized[],
-  loading: Boolean
-}
 
-interface personalizedDispatch {
-  indexFetchRequst: typeof indexFetchRequst
-}
-
-type indexProps = personalizedState & personalizedDispatch
-
-class Index extends React.Component<indexProps> {
+class Index extends React.Component {
     
   public componentDidMount() {
-    const { indexFetchRequst: fr } = this.props;
-
-    fr();
   }
 
   public render () {
-    const { data } = this.props;
     return (
         <div>
-          <div className="remd">推荐歌单</div>
-
-            <div className="remd_ul">
-              {
-                data.map(item => 
-                  <div className="remd_li">
-                    <div className="remd_img">
-                      <img src={item.picUrl} alt=""/>
-                        <span className="u-earp remd_lnum">{item.playCount}</span>
-                    </div>
-                    <p className="remd_text">{item.name}</p>
-                  </div>
-                )
-              }
-            </div>
+          <PersonalizedList />
         </div>
     )
   }
 }
 
-const mapStateToProps = ({ index } : stateObject) => ({
-    loading: index.loading,
-    data: index.data
-})
-
-const mapDispatchToProps = {
-    indexFetchRequst
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Index);
+export default Index
--- src/pages/index/personalized.tsx
+++ src/pages/index/personalized.tsx
@@ -1,17 +1,66 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Personalized } from '../../store/index/types';
+import { indexFetchRequst } from '../../store/index/action';
+import { stateObject } from '../../store'
 
+import './index.scss';
 
-class Personalized extends React.Component {
-
-    public componentDidMount() {
-    }
-    
-    public render () {
-        return (
-            <div>1</div>
-        )
-    }
+interface personalizedState {
+  data: Personalized[],
+  loading: Boolean
 }
 
+interface personalizedDispatch {
+  indexFetchRequst: typeof indexFetchRequst
+}
 
-export default Personalized
+type indexProps = personalizedState & personalizedDispatch
+
+class PersonalizedList extends React.Component<indexProps> {
+    
+  public componentDidMount() {
+    const { indexFetchRequst: fr, data } = this.props;
+    if (data.length === 0) {
+      fr();  
+    } 
+  }
+
+  public render () {
+    const { data } = this.props;
+    const personalizedList = data.splice(0, 6);
+    return (
+        <div>
+          <div className="remd">推荐歌单</div>
+
+            <div className="remd_ul">
+              {
+                personalizedList.map(item => 
+                  <div key={item.id} className="remd_li">
+                    <div className="remd_img">
+                      <img src={item.picUrl} alt=""/>
+                        <span className="u-earp remd_lnum">{item.playCount}</span>
+                    </div>
+                    <p className="remd_text">{item.name}</p>
+                  </div>
+                )
+              }
+            </div>
+        </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ index } : stateObject) => ({
+    loading: index.loading,
+    data: index.data
+})
+
+const mapDispatchToProps = {
+    indexFetchRequst
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PersonalizedList);
