94bd3eb408794bb9388f36d8a406e7ae9574a12e
Chung941211 <444192287@qq.com>
2020-05-25 17:40:33 +0800

    fix hook

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import './player.scss';
+import { useState } from 'react';
 
-class Player extends React.Component {
-    public render () {
-        return (
-            <div className="player">
-                <div className="player-header">
-                    <img src="https://p2.music.126.net/2KznZJlHqhbmpdRAZ18oDA==/109951164742110795.jpg" alt="" />
-                </div>
-                <div className="palyer-info">
-                    <div>最后的外卖</div>
-                    <div>周深</div>
-                </div>
+const Player = () => {
+    const [count, setCount] = useState(0);
+
+    
+    return (
+        <div className="player">
+            <div className="player-header">
+                <img src="https://p2.music.126.net/2KznZJlHqhbmpdRAZ18oDA==/109951164742110795.jpg" alt="" />
             </div>
-        )
-    }
+            <div className="palyer-info">
+                <div>最后的外卖</div>
+                <div>周深</div>
+            </div>
+        </div>
+    )
 }
 
 export default Player
